1156ccfcde3e975f32a7ba5a5cd889f15abb79e1
chachayena <yena.cha0916@gmail.com>
2026-03-21 07:06:41 +0900

    Initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+EMAIL = os.getenv("EMAIL")
+APP_PASSWORD = os.getenv("APP_PASSWORD")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+
+RECIPIENTS = os.getenv("RECIPIENTS")
---
+++ main.py
@@ -0,0 +1,55 @@
+import requests
+import smtplib
+from email.mime.text import MIMEText
+from config import EMAIL, APP_PASSWORD, NEWS_API_KEY, RECIPIENTS
+
+# 🇺🇸 미국 뉴스
+def get_us_news():
+    url = f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={NEWS_API_KEY}"
+    res = requests.get(url)
+    data = res.json()
+    return data.get("articles", [])[:5]
+
+# 🇰🇷 한국 뉴스
+def get_kr_news():
+    url = f"https://newsapi.org/v2/top-headlines?country=kr&category=business&apiKey={NEWS_API_KEY}"
+    res = requests.get(url)
+    data = res.json()
+    return data.get("articles", [])[:5]
+
+#  메일 내용 만들기
+def make_summary(us_news, kr_news):
+    summary = "오늘의 경제 뉴스\n\n"
+
+    summary += "🇺🇸 미국 뉴스\n"
+    for i, news in enumerate(us_news, 1):
+        summary += f"{i}. {news['title']}\n{news['url']}\n\n"
+
+    summary += "🇰🇷 한국 뉴스\n"
+    for i, news in enumerate(kr_news, 1):
+        summary += f"{i}. {news['title']}\n{news['url']}\n\n"
+
+    return summary
+
+#  이메일 발송
+def send_email(content):
+    recipients = RECIPIENTS.split(",")
+
+    msg = MIMEText(content)
+    msg["Subject"] = "한미 경제 뉴스"
+    msg["From"] = EMAIL
+    msg["To"] = ", ".join(recipients)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(EMAIL, APP_PASSWORD)
+        server.sendmail(EMAIL, recipients, msg.as_string())
+
+#  실행
+if __name__ == "__main__":
+    us_news = get_us_news()
+    kr_news = get_kr_news()
+
+    summary = make_summary(us_news, kr_news)
+    send_email(summary)
+
+    print("메일 발송 완료 ")
